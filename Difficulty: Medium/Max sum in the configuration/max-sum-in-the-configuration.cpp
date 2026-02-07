451ad139a51ac42c64bad14e32e5c5e1fb94b0a4
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-02-07 18:37:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max sum in the configuration/max-sum-in-the-configuration.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int maxSum(vector<int> &arr) {
+        int n = arr.size();
+        
+        int arrSum = 0;
+        int currVal = 0;
+        
+        for (int i = 0; i < n; i++) {
+            arrSum += arr[i];
+            currVal += i * arr[i];
+        }
+        
+        int maxVal = currVal;
+        
+        for (int i = 1; i < n; i++) {
+            currVal = currVal + arrSum - n * arr[n - i];
+            maxVal = max(maxVal, currVal);
+        }
+        
+        return maxVal;
+    }
+};
